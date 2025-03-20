d1dba577c7d733169686e71903c8175277fd9367
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2025-03-20 08:06:49 -0600

    jwt config and authFilter

--- src/main/java/com/warhe8d/config/JwtAuthenticationFilter.java
+++ src/main/java/com/warhe8d/config/JwtAuthenticationFilter.java
@@ -1,4 +1,55 @@
 package com.warhe8d.config;
 
-public class JwtAuthenticationFilter {
+import com.warhe8d.models.Users;
+import com.warhe8d.service.UsersService;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtConfig jwtConfig;
+    private final UsersService usersService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        String username = jwtConfig.getUsername(jwt);
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            Users user = usersService.findByUsername(username);
+            if(jwtConfig.isTokenValid(jwt, user)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        user, null, user.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
 }
--- src/main/java/com/warhe8d/config/JwtConfig.java
+++ src/main/java/com/warhe8d/config/JwtConfig.java
@@ -1,4 +1,70 @@
 package com.warhe8d.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
 public class JwtConfig {
+    private static final String SIGNING_KEY = "5558fbbba28bc468507aa45cbf2d74d286cf043ff4ccee9f531b5d8bc5586052";
+    public String getUsername(String jwt) {
+        return extractClaim(jwt,Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = DatatypeConverter.parseBase64Binary(SIGNING_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(Map<String,Object> extractClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *60))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+    //to generate token without using claim hashmap
+    public String generateToken(UserDetails ud){
+        return generateToken(new HashMap<>(),ud);
+    }
+
+    public boolean isTokenValid(String jwt,UserDetails ud){
+        final String userName = getUsername(jwt);
+        return (userName.equals(ud.getUsername())) && !isTokenExpired(jwt);
+    }
+
+    private boolean isTokenExpired(String jwt) {
+        return getTokenExpiry(jwt).before(new Date());
+    }
+
+    private Date getTokenExpiry(String jwt) {
+        return extractClaim(jwt,Claims::getExpiration);
+    }
 }
